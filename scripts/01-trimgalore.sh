496b3fc17d281f7b3c6b1c927d8ef62b6e62b1ce
Yaamini Venkataraman <yaaminiv@uw.edu>
2020-07-28 15:08:32 -0700

    Create 01-trimgalore.sh

---
+++ scripts/01-trimgalore.sh
@@ -0,0 +1,42 @@
+#Trim RNASeq data for downstream analyses
+#Script run in /data/users/colleenb/Oyster/00_fastq directory
+
+trim_galore \
+--paired \
+--output_dir 01_trimgalore \
+--fastqc_args \
+" 01_trimgalore/trimmed_fastqc \
+--threads 18" \
+../colleenb/Oyster/00_fastq/Cont-GP317-A1_R1_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Cont-GP317-A1_R2_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Cont-GP317-A2_R1_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Cont-GP317-A2_R2_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Cont-GP317-B1_R1_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Cont-GP317-B1_R2_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Cont-GP317-B2_R1_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Cont-GP317-B2_R2_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Cont-Lola-A1_R1_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Cont-Lola-A1_R2_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Cont-Lola-A2_R1_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Cont-Lola-A2_R2_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Cont-Lola-B1_R1_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Cont-Lola-B1_R2_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Cont-Lola-B2_R1_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Cont-Lola-B2_R2_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Exp-GP317-A1_R1_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Exp-GP317-A1_R2_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Exp-GP317-A2_R1_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Exp-GP317-A2_R2_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Exp-GP317-B1_R1_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Exp-GP317-B1_R2_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Exp-GP317-B2_R1_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Exp-GP317-B2_R2_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Exp-Lola-A1_R1_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Exp-Lola-A1_R2_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Exp-Lola-A2_R1_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Exp-Lola-A2_R2_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Exp-Lola-B1_R1_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Exp-Lola-B1_R2_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Exp-Lola-B2_R1_001.fastq.gz \
+../colleenb/Oyster/00_fastq/Exp-Lola-B2_R2_001.fastq.gz \
+2> stderr.log
