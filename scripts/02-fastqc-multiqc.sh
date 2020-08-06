de1dec08b86fb7ed40040b93f7595484514a5081
Yaamini Venkataraman <yaaminiv@uw.edu>
2020-08-06 14:05:51 -0700

    added code for untrimmed scripts

--- scripts/02-fastqc-multiqc.sh
+++ scripts/02-fastqc-multiqc.sh
@@ -1,5 +1,27 @@
-#Run FastQC and MultiQC on trimmed data
+#Run FastQC and MultiQC on untrimmed and trimmed data
 #Script run in /data/users/yaamini/
+
+#Untrimmed samples
+
+#Path to FastQC
+#Path to trimmed files
+#Parallel threads to run
+#Output directory
+
+~/FastQC/fastqc \
+/data/users/colleenb/Oyster/00_fastq/*gz \
+-t 18 \
+-o ~/01_trimgalore/untrimmed_fastqc
+
+#Path to MultiQC 
+# Directory with FastQC output
+#MultiQC output directory
+
+multiqc \
+~/01_trimgalore/untrimmed_fastqc \
+-o ~/01_trimgalore/untrimmed_fastqc/multiqc
+
+#Trimmed samples
 
 #Path to FastQC
 #Path to trimmed files
