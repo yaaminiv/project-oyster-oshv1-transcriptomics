6edea5bce19ce90286f3e06e8888f008191dd2d4
Yaamini Venkataraman <yaaminiv@uw.edu>
2020-08-06 12:57:45 -0700

    changed path for fastqc and fixed multiqc -o

--- scripts/02-fastqc-multiqc.sh
+++ scripts/02-fastqc-multiqc.sh
@@ -1,20 +1,20 @@
 #Run FastQC and MultiQC on trimmed data
 #Script run in /data/users/yaamini/
 
-#FastQC
+#Path to FastQC
 #Path to trimmed files
 #Parallel threads to run
 #Output directory
 
-/usr/bin/fastqc \
+~/FastQC/fastqc \
 ~/01_trimgalore/*gz \
 -t 18 \
 -o ~/01_trimgalore/trimmed_fastqc
 
-#MultiQC 
+#Path to MultiQC 
 # Directory with FastQC output
 #MultiQC output directory
 
 multiqc \
 ~/01_trimgalore/trimmed_fastqc \
-> --outdir ~/01_trimgalore/trimmed_fastqc/multiqc
+-o ~/01_trimgalore/trimmed_fastqc/multiqc
